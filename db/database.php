99ced211b71b6f4494915c22b0c92799841bc11b
ormux <ormuseldesu@gmail.com>
2022-05-13 18:22:19 +1000

    enable PDO exceptions

--- db/database.php
+++ db/database.php
@@ -1,6 +1,6 @@
-<?php 
-include('pdo.php');
+<?php include('db/pdo.php');
 $pdo = ( new MySQLConnection() )->connect();
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if($pdo === null) {
    print "Could not connect to DB";
 } else {
